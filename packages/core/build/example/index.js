207997cec9b90675cfda699b1cc8226d43fa9742
Giang Huynh <trgianghuynhdev1808@gmail.com>
2023-11-22 14:20:57 +0700

    init: setup core & vue-app workspaces

---
+++ packages/core/build/example/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.example = void 0;
+function example() {
+    return "example";
+}
+exports.example = example;
+//# sourceMappingURL=index.js.map
